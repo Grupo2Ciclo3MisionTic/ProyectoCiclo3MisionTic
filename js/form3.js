3a2589ea72edd3fc49083b988821a2e665579a1c
marlon <mgagudelot@gmail.com>
2021-09-24 11:20:55 -0500

    Se realizan alguna mejoras esteticas y en el codigo

--- js/form3.js
+++ js/form3.js
@@ -1,33 +1,39 @@
 function validar_telefono(string){
     // defincion de variale y asignación de valor que si esta valido el parametro
-    var valido = 1;
+    var valido = true;
     var msg = "El telefono ingresado esta correcto...";
         
     // validando la longitud minina
     if (string.length < 6) {
         // estableciendo que no es valido
-        valido = 0;
+        
         msg = "Debe tener una longitud mayor a 6 ";
+        valido =false;
     }
     // validando la longitud minina
-    if (valido == 1 && string.length > 10) {
+    if (valido == true && string.length > 10) {
         // estableciendo que no es valido
-        valido = 0;
+        
         msg = "Debe tener una longitud máxima de 10 caracteres";
+        valido =false;
+        
     }
     if (
-        (valido == 1 && typeof string != "string")
+        (valido == true && typeof string != "string")
         ||
-        (valido == 1 && isNaN(string))
+        (valido == true && isNaN(string))
        )
     {
         // estableciendo que no e// estableciendo que no es valido
-        valido = 0;
+        
         msg = "Debe ingresar un telefono con sólo digitos";
+        valido =false;
+        
     }
     if( /^\s+$/.test(string) ) {
-        valido =0;
+
         msg = "No puede tener ningún carácter especial como: ‘@’, ‘!’,’_’,’-’,’/";
+        valido =false;
       }
     // mostrando un mensaje del resultado obtenido
     alert(msg);
@@ -38,31 +44,35 @@ function validar_telefono(string){
 
 function validar_direccion(string){
          
-        var valido2 = 1;
+        var valido2 = true;
         var msg = "Direccion ingresada de forma correcta";
    
         if ((!string.startsWith('Carrera')) && (!string.startsWith('Transversal'))  && (!string.startsWith('Circular')) && (!string.startsWith('Calle')))
            {
                // estableciendo que no es valido
-               valido2 = 0;
-               msg = "La direccion debe iniciar con Carrera o con Transversal o con Circular o con Calle";
+               
+                msg = "La direccion debe iniciar con Carrera o con Transversal o con Circular o con Calle";
+                valido2 =false;
            }
            if (string.length < 7) {
             // estableciendo que no es valido
-            valido2 = 0;
-            msg = "Debe tener una longitud mínima de 7 caracteres.";
+            
+                msg = "Debe tener una longitud mínima de 7 caracteres.";
+                valido2 =false;
            }
            // validando la longitud minina
-           if (valido2 == 1 && string.length > 30) {
+           if (valido2 == true && string.length > 30) {
                // estableciendo que no es valido
-               valido2 = 0;
+               
                msg = " Debe tener una longitud máxima de 30 caracteres.";
+               valido2 =false;
            }
 
            if (!string.match(/#/)){
                
-                valido2 = 0;
+                
                 msg = "Debe contener el carácter ‘#’ en algún lugar de la cadena";
+                valido2 =false;
 
            }
 

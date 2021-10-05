f97792023a2cf0bfba5db441fdb512443283f60e
marlon <mgagudelot@gmail.com>
2021-10-04 20:04:50 -0500

    Se realizan cambios en regristro3

--- js/registro3.js
+++ js/registro3.js
@@ -1,32 +1,74 @@
-function buscarDominio(arreglo){
-    var args = [];
-    for (var i = 0; i< arreglo.length; i++){
 
-        if(arreglo[i].campoCorreoElectronico.split("@")[1] == "upb.edu.co"){
-        
-        args.push(arreglo);
-        }
-    }
-    return(args);
+var campoCorreo = "";
+var campoNombre = "";
+var campoTelefono = "";
+var arreglo = [];
+var args = [];
+
+
+//var formulario = document.getElementById("formRegistro");
+
+function registros (){
+    var datos = {
+        campoCorreo: campoCorreo,
+        campoNombre: campoNombre,
+        campoTelefono: campoTelefono,
+    };
+
+    arreglo.push(datos);
+    console.log(arreglo);
 }
 
+//registro();
+//console.log(arreglo);
+
+
+function buscarDominio(arreglo){
+    
+    console.log(arreglo);
+    console.log(typeof arreglo);
+
+    for(var i = 0; i<arreglo.length; i++){
+
+        if(arreglo[i].campoCorreo.split("@")[1] == "upb.edu.co"){
+        
+        args.push(arreglo);
+
+        console.log(args);
+
+        }
+        return args;
+    }
+    
+}
+
+//buscarDominio(arreglo);
+//console.log(args);
+
 function retornarCuenta(args){
-    var ocurrencias = 0;
+
+    var ocurrencias = 0; 
+
     for (var i= 0; i<args.length; i++){
 
-        if(args[i].campoNombre.starsWith("a")){
+        args = args[i]
+
+        console.log(typeof campoNombre)
+        if(campoNombre.startsWith("a")){
             
-            if(args[i].campoTelefono.endsWith("0") || args[i].campoTelefono.endsWith("4")){
+            if(campoTelefono.endsWith("0") || campoTelefono.endsWith("4")){
                 
                 ocurrencias ++;
                 console.log(ocurrencias);
             }
 
         }
+        return ocurrencias; 
     }
-    return ocurrencias;  
-    
-}
+}   
+//retornarCuenta(args);
 
+
+module.exports.registro = registros();
 module.exports.buscarDominio = buscarDominio();
 module.exports.retornarCuenta = retornarCuenta();
